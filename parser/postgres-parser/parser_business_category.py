8ecc773e03ba66844eea857fe6e4c8c82393ba98
manoj <manojpr4@gmail.com>
2017-02-15 17:33:55 -0700

    Business category parser

---
+++ parser/postgres-parser/parser_business_category.py
@@ -0,0 +1,27 @@
+# Parse business categories each business is listed under into a Postgres database. If a business is listed under three different categories then the categories will be parsed into three different rows.
+
+# Import Psycopg Postgres adapter for Python
+import psycopg2
+
+# Import Python's default JSON encoder and decoder
+import json
+
+# Open a connection to the database
+db_conn = psycopg2.connect("dbname='database_name' host='server_address' user='username' password='password'")
+
+# Initialize an empty list for temporarily holding JSON data
+data = []
+
+# Open a JSON file from the dataset and read data line-by-line iteratively
+with open('yelp_academic_dataset_business.json') as fileobject:
+	for line in fileobject:
+		data = json.loads(line)
+		# Parse the JSON object in your database
+		i=0
+		while data['categories'] is not None and i<len(data['categories']):
+			cur = db_conn.cursor()
+			cur.execute("insert into business_category (business_id, category) values(%s, %s)", (data['business_id'], data['categories'][i]))
+			db_conn.commit()
+			print(data['business_id'])
+			i += 1
+		
