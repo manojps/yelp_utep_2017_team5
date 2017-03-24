48f0ca5c7751f909420c58f0a544bfc343acb39f
Manoj Saha <manojpr4@gmail.com>
2017-03-24 01:33:25 -0600

    Location plot using gmplot

---
+++ location_plot/business_location_gmplot.py
@@ -0,0 +1,28 @@
+# Author: Manoj Pravakar Saha
+# This work is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International License.
+
+import psycopg2
+import gmplot
+db_conn = psycopg2.connect("dbname='database' host='host_address' user='username' password='password'")
+cur = db_conn.cursor()
+cur.execute("select latitude, longitude from business where postal_code='89109';")
+lat_long = cur.fetchall()
+
+latitude = []
+longitude = [] 
+
+for i in range(len(lat_long)):
+	latitude.append(lat_long[i][0])
+	longitude.append(lat_long[i][1])
+
+gmap = gmplot.GoogleMapPlotter(36.1215,-115.1696, 13)
+gmap.scatter(latitude, longitude, '#FF6666', edge_width=10)
+gmap.draw('Code/yelp_local/map.html')
+
+cur.close()
+db_conn.close()
+
+
+
+
+
