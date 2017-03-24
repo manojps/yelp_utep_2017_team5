01b6dfd0d66760e97dbbf62e830057d37745bebb
Manoj Saha <manojpr4@gmail.com>
2017-03-24 02:46:45 -0600

    User location crawler

---
+++ user_location/user_location_crawler.py
@@ -0,0 +1,36 @@
+# Author: Manoj Pravakar Saha
+# This work is licensed under the Attribution-NonCommercial-ShareAlike 4.0 International License.
+
+from lxml import html
+import requests
+import psycopg2
+import json
+
+conn = psycopg2.connect("dbname='database' host='host_address' user='username' password='password'")
+
+url_seed = 'https://www.yelp.com/user_details?userid='
+
+with open('yelp_academic_dataset_user.json') as fileobject:
+	for line in fileobject:
+		data = json.loads(line)
+		url = 'https://www.yelp.com/user_details?userid='+data['user_id']
+
+		page = requests.get(url)
+		tree =  html.fromstring(page.text)
+		user_location = tree.xpath('//*[@class="user-location alternate"]/text()')
+		location = user_location[0].split(', ')
+
+		if (len(location) < 2):
+			location.append(location [0])
+			location[0] = ''
+
+		if (len(location) > 2):
+			location [0] = location[1]
+			location[1] = location[2]
+
+		cur = conn.cursor()
+		cur.execute("insert into user_location (user_id, user_city, user_state_country) values (%s, %s, %s)", (data['user_id'], location[0], location[1]))
+		conn.commit()
+
+cur.close()
+conn.close()
